c4b218a715b6466c40d15e6767054cabc4381615
Xys-Tus <Sixtus Onyedibe>
2022-10-29 18:52:52 +0100

    Created Home component

---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className=" w-full h-screen flex justify-center pt-[20px]">
+      <div className="font-bold w-[88px] h-[88px] bg-black rounded "></div>
+    </div>
+  );
+};
+
+export default Home;
