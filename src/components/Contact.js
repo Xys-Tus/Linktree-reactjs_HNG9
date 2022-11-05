ab3d55b328e7d09cbc753e66276177db7700fbf6
Xys-Tus <Sixtus Onyedibe>
2022-11-05 07:40:57 +0100

    Bug: FIxed that made the build to fail.

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,45 +1,40 @@
 import React from "react";
-import Backbtn from "../Images/backBtn.png";
-import Slack from "../Images/slack.png";
-import Github from "../Images/github.png";
 import Zuri from "../Images/Zuri.png";
 import I4G from "../Images/I4G.png";
-import Profile_img from "../Images/profile_img.png";
-import ThreeDots from "../Images/threedots.png";
 
 const Home = () => {
   return (
     <>
-      <div className=" w-full flex items-center pt-[100px] flex-col h-screen">
-        <div className="w-[70%] mx-auto">
-          <div className="w-full mb-[10px]">
+      <div className=" w-full flex items-center pt-[64px] flex-col h-screen">
+        <div className="md:w-[70%] md:mx-auto w-full px-[20px] mb-[20px]">
+          <div className="w-full mb-[30px]">
             <h1 className=" text-[2rem] font-semibold ">Contact Me</h1>
-            <p>
+            <p className="text-[#475467]">
               Hi there, contact me to ask me about anything you have in mind.
             </p>
           </div>
           <form>
-            <div className="flex justify-between w-full ">
-              <div className="w-full mr-[10px]">
+            <div className="flex flex-col md:flex-row justify-between w-full ">
+              <div className="w-full mr-[10px] mb-[20px] md:mb-0">
                 <label htmlFor="first_name" className=" font-semibold ">
                   First name
                 </label>
                 <input
                   type="text"
                   id="first_name"
-                  placeholder=" Enter your first name"
-                  className="border h-[40px] w-full outline-blue-300"
+                  placeholder="Enter your first name"
+                  className="border h-[44px] w-full outline-blue-300 px-[10px]"
                 />
               </div>
-              <div className="w-full mb-[20px] flex-col justify-end ml-[10px]">
+              <div className="w-full mb-[20px] flex-col justify-end md:ml-[10px]">
                 <label htmlFor="last_name" className="font-semibold ">
                   Last name
                 </label>
                 <input
                   type="text"
                   id="last_name"
-                  placeholder=" Enter your last name"
-                  className="border h-[40px] rounded-[5px] w-[100%] outline-blue-300"
+                  placeholder="Enter your last name"
+                  className="border h-[44px] rounded-[5px] w-[100%] outline-blue-300 px-[10px]"
                 />
               </div>
             </div>
@@ -50,8 +45,8 @@ const Home = () => {
               <input
                 type="text"
                 id="email"
-                placeholder=" youremail@email.com"
-                className="w-full border h-[40px] rounded-[5px] outline-blue-300"
+                placeholder="youremail@email.com"
+                className="w-full border h-[44px] rounded-[5px] outline-blue-300 px-[10px]"
               />
             </div>
             <div className="message mb-[20px]">
@@ -59,8 +54,8 @@ const Home = () => {
                 Message
               </label>
               <textarea
-                placeholder=" Send me a message and I'll reply you as soon as possible..."
-                className="w-full border rounded-[5px] h-[200px] focus:border-blue-300 outline-blue-300 resize-none"
+                placeholder="Send me a message and I'll reply you as soon as possible..."
+                className="w-full border rounded-[5px] h-[132px] focus:border-blue-300 outline-blue-300 resize-none p-[10px]"
               />
             </div>
             <div className="flex mb-[20px]">
@@ -79,7 +74,7 @@ const Home = () => {
           </form>
         </div>
       </div>
-      <div className="px-[20px] md:px-[40px] mb-[50px] md:mb-0 mt-[100px]">
+      <div className="px-[20px] md:px-[40px] mb-[50px] md:mb-0 mt-[96px]">
         <footer className="flex justify-between md:items-center flex-col md:flex-row border-t h-[20vh]">
           <div className="zuri my-[20px] md:my-0">
             <img src={Zuri} alt="" />
